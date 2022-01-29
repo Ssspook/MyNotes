78a2a916eb4ea6552e667b34e07e2d610e907e13
Mike Andrew <mikeandrew2016@gmail.com>
2022-01-29 13:00:40 +0300

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Meritocrat on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
